fefcb1d7cd8bbf18417015e2238cda4f957915ff
conscab <constantin.cabiniuc@gmail.com>
2019-05-31 18:12:05 +0300

    first commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 8f, maxVelocity = 4f;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0)
+        {
+            if (vel < maxVelocity)
+                forceX = speed;
+            anim.SetBool("Walk", true);
+
+            Vector3 temp = transform.localScale;
+            temp.x = 1.3f;
+            transform.localScale = temp;
+
+        }
+        else if (h < 0)
+        {
+            if (vel < maxVelocity)
+                forceX = -speed;
+            anim.SetBool("Walk", true);
+
+            Vector3 temp = transform.localScale;
+            temp.x = -1.3f;
+            transform.localScale = temp;
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+        myBody.AddForce(new Vector2(forceX, 0));
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMoveKeyboard();
+    }
+}
