7be91272789f54918b9fb35384f332c0bc828e10
conscab <constantin.cabiniuc@gmail.com>
2019-10-20 23:14:54 +0300

    menu scripts added

---
+++ Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Gameplay");
+    }
+
+
+    public void HighscoresMenu()
+    {
+        Application.LoadLevel("HighScore");
+    }
+
+    public void OptionsMenu()
+    {
+        Application.LoadLevel("OptionsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+
+    public void MusicButton()
+    {
+
+    }
+}
